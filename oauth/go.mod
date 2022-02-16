dc452500ba78e14922e4dd61d2ed5d0132b65a25
Esequiel <virtuosoesequiel@gmail.com>
2022-02-16 16:12:34 -0300

    Add oauth client lib.

---
+++ oauth/go.mod
@@ -0,0 +1,8 @@
+module github.com/esequielvirtuoso/oauth_go_lib/oauth
+
+go 1.16
+
+require (
+	github.com/esequielvirtuoso/go_utils_lib/rest_errors v0.0.0-20220215213401-671ebc7be6da
+	github.com/mercadolibre/golang-restclient v0.0.0-20170701022150-51958130a0a0
+)
---
+++ oauth/oauth.go
@@ -0,0 +1,125 @@
+package oauth
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	restErrors "github.com/esequielvirtuoso/go_utils_lib/rest_errors"
+	"github.com/mercadolibre/golang-restclient/rest"
+)
+
+const (
+	// This X-Public header is not being set by the client. It will be set by the NGINX server
+	// based on the condition if the request was generated from outside the network or inside the network.
+	headerXPublic   = "X-Public"
+	headerXClientID = "X-Client_Id"
+	headerXCallerID = "X-Caller-Id"
+
+	paramAccessToken = "access_token"
+)
+
+var (
+	oauthRestClient = rest.RequestBuilder{
+		BaseURL: "http://localhost:5002",
+		Timeout: 200 * time.Millisecond,
+	}
+)
+
+type accessToken struct {
+	ID       string `json:"id"`
+	UserID   int64  `json:"user_id"`
+	ClientID int64  `json:"client_id"`
+}
+
+func IsPublic(request *http.Request) bool {
+	if request != nil {
+		// this means it is a public request
+		return true
+	}
+
+	return request.Header.Get(headerXPublic) == "true"
+}
+
+func GetCallerId(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+	callerID, err := strconv.ParseInt(request.Header.Get(headerXCallerID), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return callerID
+}
+
+func GetClientId(request *http.Request) int64 {
+	if request == nil {
+		return 0
+	}
+	clientID, err := strconv.ParseInt(request.Header.Get(headerXClientID), 10, 64)
+	if err != nil {
+		return 0
+	}
+	return clientID
+}
+
+func AuthenticateRequest(request *http.Request) restErrors.RestErr {
+	if request == nil {
+		// this means it is a public request
+		return nil
+	}
+
+	cleanRequest(request)
+
+	// http://localhost:8082/resource?access_token=abc13
+	accessTokenID := strings.TrimSpace(request.URL.Query().Get(paramAccessToken))
+	if accessTokenID == "" {
+		return nil
+	}
+
+	at, err := getAccessToken(accessTokenID)
+	if err != nil {
+		if err.Status() == http.StatusNotFound {
+			return nil
+		}
+		return err
+	}
+
+	request.Header.Add(headerXCallerID, fmt.Sprintf("%v", at.UserID))
+	request.Header.Add(headerXClientID, fmt.Sprintf("%v", at.ClientID))
+
+	return nil
+}
+
+func cleanRequest(request *http.Request) {
+	if request == nil {
+		return
+	}
+
+	request.Header.Del(headerXClientID)
+	request.Header.Del(headerXCallerID)
+}
+
+func getAccessToken(accessTokenID string) (*accessToken, restErrors.RestErr) {
+	response := oauthRestClient.Get(fmt.Sprintf("/oauth/access_token/%s", accessTokenID))
+	if response == nil || response.Response == nil {
+		return nil, restErrors.NewInternalServerError("invalid rest client response when trying to get access token")
+	}
+	if response.StatusCode > 299 {
+		var restErr restErrors.RestErr
+		if err := json.Unmarshal(response.Bytes(), &restErr); err != nil {
+			return nil, restErrors.NewInternalServerError("invalid error interface when trying to get access token")
+		}
+
+		return nil, restErr
+	}
+
+	var at accessToken
+	if err := json.Unmarshal(response.Bytes(), &at); err != nil {
+		return nil, restErrors.NewInternalServerError("error when trying to unmarshal access token response")
+	}
+	return &at, nil
+}
